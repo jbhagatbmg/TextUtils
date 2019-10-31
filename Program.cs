09c791df66d3f40b44e0bf1a3c1db89b538bb906
jbhagat <jbhagat@bmgdigitaltech.com>
2019-10-31 13:09:50 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text.Encodings.Web;
+using System.Web;
+using TextCopy;
+
+namespace TextUtils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int func = -1;
+            do
+            {
+                try
+                {
+                    string menu = @"
+            Clipboard Transform Actions:
+
+            -1: Quit
+            1: Base64 Decode
+            2: Base64 Encode
+            3: HTML Decode
+            4: HTML Encode
+            5: Unique Difference In Lists
+            ";
+                    Console.WriteLine(menu);
+                    Console.Write("Enter Action: ");
+                    func = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    switch (func)
+                    {
+                        case 1:
+                            Clipboard.SetText(TextUtilities.DecodeBase64(Clipboard.GetText()));
+                            break;
+                        case 2:
+                            Clipboard.SetText(TextUtilities.EncodeBase64(Clipboard.GetText()));
+                            break;
+                        case 3:
+                            Clipboard.SetText(HttpUtility.HtmlDecode(Clipboard.GetText()));
+                            break;
+                        case 4:
+                            Clipboard.SetText(HtmlEncoder.Default.Encode(Clipboard.GetText()));
+                            break;
+                        case 5:
+                            Console.Write("Confirm paste first set [ENTER]:");
+                            Console.ReadLine();
+                            string _set1 = Clipboard.GetText();
+                            Console.Write("Confirm paste second set [ENTER]:");
+                            Console.ReadLine();
+                            string _set2 = Clipboard.GetText();
+                            Clipboard.SetText(TextUtilities.UniqueDiffLines(_set1, _set2));
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            } while (func != -1);
+        }
+    }
+}
---
+++ TextUtilities.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TextUtils
+{
+    public static class TextUtilities
+    {
+        public static string DecodeBase64(string input)
+        {
+            byte[] data = Convert.FromBase64String(input);
+            return Encoding.UTF8.GetString(data);
+        }
+
+        public static string EncodeBase64(string text)
+        {
+            return (Convert.ToBase64String(Encoding.UTF8.GetBytes(text), Base64FormattingOptions.None));
+        }
+        public static string UniqueDiffLines(string text1, string text2)
+        {
+            var _list1 = text1.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Select(a => a.ToUpperInvariant()).ToList();
+            var _list2 = text2.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Select(a => a.ToUpperInvariant()).ToList();
+            Console.WriteLine($"List 1 Length: {_list1.Count}");
+            Console.WriteLine($"List 2 Length: {_list2.Count}");
+            Console.WriteLine($"Diff Length: {Math.Abs(_list1.Count - _list2.Count)}");
+
+            return string.Join(Environment.NewLine, _list1.Except(_list2).Concat(_list1.Except(_list2)).Distinct());
+        }
+
+    }
+}
